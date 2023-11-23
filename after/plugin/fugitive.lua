0ee018689c6e361dcbd3ae82be3948b0f0cc6173
Swatar Ianess <swatarianess@gmail.com>
2023-11-24 00:03:21 +0100

    removed the unneeded extra logic

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,28 +1,3 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-local Swatari_Fugitive = vim.api.nvim_create_augroup("Swatari_Fugitive", {})
 
-local autocmd = vim.api.nvim_create_autocmd
-autocmd("BufWinEnter", {
-    group = Swatari_Fugitive,
-    pattern = "*",
-    callback = function ()
-        if vim.bo.ft ~= "fugitive" then
-            return
-        end
-
-        local bufnr = vim.api.nvim_get_current_bug()
-        local opts = {buffer = bufnr, remap = false}
-        vim.keymap.set("n", "<leader>p", function ()
-            vim.cmd.Git('push')
-        end, opts)
-
-        -- rebase always
-        vim.keymap.set("n", "<leader>P", function ()
-            vim.cmd.Git({"pull", "--rebase"})
-        end, opts)
-        -- Note: It allows me to easily set the branch i am pushing and any 
-        -- tracking needed if I did not set the branch up correctly
-        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
-    end,
-})
