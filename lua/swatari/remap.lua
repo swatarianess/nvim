7f85073e32b47dfb8453e70aa763f6b77268aa82
Swatar Ianess <swatarianess@gmail.com>
2023-12-13 10:30:48 +0100

    Update different files

--- lua/swatari/remap.lua
+++ lua/swatari/remap.lua
@@ -11,7 +11,6 @@ vim.keymap.set("n", "<leader>Y", [["+Y"]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
     print("ShoutOut!")
