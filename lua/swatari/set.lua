c0b9a1a6328042f3702df0ea84f1475faa263c3c
swatari@homelab <swatarianess@gmail.com>
2025-01-13 19:42:24 +0100

    Fixed the issue with sourcing the .vimrc file

--- lua/swatari/set.lua
+++ lua/swatari/set.lua
@@ -1,8 +1,14 @@
-vim.cmd('source ~/.vimrc')
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+
+-- Other settings
+vim.opt.number = true
+vim.opt.autoindent = true
+vim.opt.cursorcolumn = true
+vim.opt.cursorline = true
 
 vim.opt.smartindent = true
 vim.opt.wrap = false
