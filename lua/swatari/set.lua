6b54d7a67f63317017611bdb98fa8e55ccb5b727
Swat Arianess <swatarianess@gmail.com>
2025-05-08 20:39:57 +0200

    Added modified set.lua

--- lua/swatari/set.lua
+++ lua/swatari/set.lua
@@ -24,6 +24,24 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
+-- Tab appearance settings
+vim.opt.showtabline = 1 -- Always show tabline
+vim.api.nvim_set_hl(0, 'TabLine', { bg = 'NONE', fg = "#565f89" })
+vim.api.nvim_set_hl(0, 'TabLineFill', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'TabLineSel', { bg = '#1a1b26', fg = '#7aa2f7', bold = true })
+
+-- Window transparency settings
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'NONE' }) -- This is for non-current (inactive) windows
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' })
+
+-- Ensure consistent transparency
+vim.opt.pumblend = 0  -- Keep popup menu solid
+vim.opt.winblend = 0  -- Keep active window solid
+
+-- Set winblend for inactive windows
+vim.opt.winblend = 80
+
 --vim.opt.scrolloff = 9
 --vim.opt.signcolumn = "yes"
 
