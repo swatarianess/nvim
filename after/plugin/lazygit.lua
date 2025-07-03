3ece14723da5414cea627b1284bd60385bf4580b
Swat Arianess <swatarianess@gmail.com>
2025-07-03 20:30:15 +0200

    Added updates and some configuration

--- after/plugin/lazygit.lua
+++ after/plugin/lazygit.lua
@@ -1,1 +1,7 @@
 vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>")
+
+
+vim.g.lazygit_floating_window_winblend = 0 -- transpareny of floating window
+-- vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
+vim.g.lazygit_floating_window_use_plenary = 0 -- Use plenary.nvim to manage floating window if available
+
