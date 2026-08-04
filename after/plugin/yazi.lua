26c9a4b5b128f5bfde5d8347f445ee4c211356e3
Stephen Adu <stephen.adu@assetcare.nl>
2026-08-04 13:25:10 +0200

    Move Yazi toggle to <leader>yy instead of Ctrl+Shift+Up
    Ctrl+Shift+Up isn't reliably sent by terminals; leader keys are more
    portable and consistent with the other Yazi mappings.

--- after/plugin/yazi.lua
+++ after/plugin/yazi.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n","<leader>-","<cmd>Yazi<cr>", { desc = "Yazi: open at file" })
 vim.keymap.set("n","<leader>cw","<cmd>Yazi cwd<cr>", { desc = "Yazi: open at cwd" })
-vim.keymap.set("n","<C-S-Up>","<cmd>Yazi toggle<cr>", { desc = "Yazi: toggle" })
+vim.keymap.set("n","<leader>yy","<cmd>Yazi toggle<cr>", { desc = "Yazi: toggle (resume)" })
