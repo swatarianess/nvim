f6e49a6b3bd4b3263febc0bd52ed4234bed4e24c
Stephen Adu <stephen.adu@assetcare.nl>
2026-08-04 13:26:08 +0200

    Move Yazi toggle to <leader>e to avoid <leader>y yank clash
    <leader>yy caused a pause on <leader>y (system-clipboard yank) while nvim
    waited for a possible second key. <leader>e is the conventional explorer
    key and conflict-free.

--- after/plugin/yazi.lua
+++ after/plugin/yazi.lua
@@ -1,3 +1,3 @@
 vim.keymap.set("n","<leader>-","<cmd>Yazi<cr>", { desc = "Yazi: open at file" })
 vim.keymap.set("n","<leader>cw","<cmd>Yazi cwd<cr>", { desc = "Yazi: open at cwd" })
-vim.keymap.set("n","<leader>yy","<cmd>Yazi toggle<cr>", { desc = "Yazi: toggle (resume)" })
+vim.keymap.set("n","<leader>e","<cmd>Yazi toggle<cr>", { desc = "Yazi: toggle (resume)" })
